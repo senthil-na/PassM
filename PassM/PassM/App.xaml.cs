d7c3a24efeef35559284b40c2f3303f8ce1a7e19
seninfothil <senthil@senthilmac.local>
2016-09-20 15:59:55 +0530

    Adding Source Code
    Adding Source Code

---
+++ PassM/PassM/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace PassM
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+			var fPage = new PassMPage();
+			MainPage = new NavigationPage(new PassMPage());
+			NavigationPage.SetBackButtonTitle(fPage, "Test");
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ PassM/PassM/PassMPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PassM
+{
+	public partial class PassMPage : ContentPage
+	{
+		public PassMPage()
+		{
+			InitializeComponent();
+		}
+
+		async void doLogin(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new ListCategory(this));
+		}
+	}
+}
+
